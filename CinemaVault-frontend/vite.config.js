784feb7c0f684a68e9d682785db81e34bec105fa
Jesus Manuel Lizarraga Sabedra <a20490710@itmexicali.edu.mx>
2025-10-21 21:28:57 -0600

    Tailwindcss Added to the project

--- CinemaVault-frontend/vite.config.js
+++ CinemaVault-frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
