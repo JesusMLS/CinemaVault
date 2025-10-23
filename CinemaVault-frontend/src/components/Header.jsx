d79ae3e64f44b63d31de91f01e41f30a38626907
Jesus Manuel Lizarraga Sabedra <a20490710@itmexicali.edu.mx>
2025-10-23 16:03:56 -0600

    Small changes to navigation bar

--- CinemaVault-frontend/src/components/Header.jsx
+++ CinemaVault-frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({ userInfo, logOut }){
         const [loading, setLoading] = useState(true);
         const [error, setError] = useState(null)
 
-        //Request N1 (Obtain List of Top 10 genres ordered by movies asc and title asc)
+        //Request N1 (Obtain a List of Top 10 genres ordered by movies asc and title asc)
         useEffect(()=>{
         API.get('/genres/?take=10&order=movies_asc').then((response)=>{
             setGenres(response.data)
@@ -24,30 +24,35 @@ function Header({ userInfo, logOut }){
         })
         }, [])
         
-        //Create an Array of Components to render
+        //Create an Array of Genres Links to render V1 (not expanded menu)
         const genresComponents = genres.map((genre) =>{
-            return <a href={'/movies/genre/'+genre.name} key={genre.id} onClick={() =>toggleMenu('menu-genres-container')}>{genre.name} {genre._count.movies}</a>
+            return <Link to={'/movies/genre/'+genre.name} key={genre.id} onClick={() =>toggleMenu('menu-genres-container')}>{genre.name} {genre._count.movies}</Link>
         })
-        
-        //Create an array of Past 10 Years
+
+        //Create an Array of Genres Links to render V2 (expanded menu)
+        const genresComponentsExpanded = genres.map((genre) =>{
+            return <Link to={'/movies/genre/'+genre.name} key={genre.id} onClick={() =>toggleMenu('menu-extended-container')}>{genre.name} {genre._count.movies}</Link>
+        })
+
+        //Create an array of past 10 Years, starting from the actual year
         const actualYear = new Date().getFullYear()
         const yearsArray = [actualYear, actualYear-1, actualYear-2, actualYear-3, actualYear-4, actualYear-5, actualYear-6, actualYear-7, actualYear-8, actualYear-9]
 
-        //Create an array of Components year to render
+        //Create an array of Years Links to render V1 (not expanded menu)
         const yearsComponents = yearsArray.map((year, index) =>{
-                                return <a href={'/movies/year/'+year} key={index} onClick={() => toggleMenu('menu-years-container')}>{year}</a>
+                                return <Link to={'/movies/year/'+year} key={index} onClick={() => toggleMenu('menu-years-container')}>{year}</Link>
+                            })
+
+        //Create an array of Years Links to render V2 (expanded menu)
+        const yearsComponentsExpanded = yearsArray.map((year, index) =>{
+                                return <Link to={'/movies/year/'+year} key={index} onClick={() => toggleMenu('menu-extended-container')}>{year}</Link>
                             })
 
         const handleLogout = () =>{
             logOut();
         }
 
-        function toggleSidebar(){
-            let sidebarContainer = document.getElementById('menu-categories-container')
-            sidebarContainer?.classList.toggle('hidden')
-            document.body.classList.toggle('overflow-hidden')
-        }
-
+        //Function to toggle a menu. You need to pass a menuId to the parameters of the function.
         function toggleMenu(menuId){
             let menuContainer = document.getElementById(menuId)
             menuContainer?.classList.toggle('hidden')
@@ -65,11 +70,11 @@ function Header({ userInfo, logOut }){
                 <p className='text-lg'>CinemaVault</p>
             </div>
             </Link>
-            {/* Links can only appear when window size is >=sm */}
-            <div className='hidden sm:flex sm:flex-row sm:gap-7'>
-            <a href='/home'>Home</a>
-            <a href='/add/movie'>Add Movie</a>
-            <a href='/movies'>Movies</a>
+            {/* Links can only appear when window size is >=md */}
+            <div className='hidden md:flex md:flex-row md:gap-7'>
+            <Link to='/home'>Home</Link>
+            <Link to='/add/movie'>Add Movie</Link>
+            <Link to='/movies'>Movies</Link>
             {/* Categories Container*/}
             <div className='relative'>
             {/* Categories Button*/}
@@ -79,11 +84,11 @@ function Header({ userInfo, logOut }){
             {/* Categories Menu*/}
             <div id='menu-categories-container' className='hidden absolute w-auto h-auto top-6 right-0 bg-amber-100 shadow-lg rounded-sm'>
                     <div className='p-3 h-full text-base flex flex-col gap-5 text-center'>
-                        <a href='/movies/category/views' onClick={() =>toggleMenu('menu-categories-container')}>User Views</a>
-                        <a href='/movies/category/posted' onClick={() =>toggleMenu('menu-categories-container')}>Latest Posts</a>
-                        <a href='/movies/category/released' onClick={() =>toggleMenu('menu-categories-container')}>Latest Releases</a>
-                        <a href='/movies/category/featured' onClick={() =>toggleMenu('menu-categories-container')}>Featured by Administrators</a>
-                        <a href='/movies/category/califications' onClick={() =>toggleMenu('menu-categories-container')}>Best Califications</a>
+                        <Link to='/movies/category/views' onClick={() =>toggleMenu('menu-categories-container')}>User Views</Link>
+                        <Link to='/movies/category/posted' onClick={() =>toggleMenu('menu-categories-container')}>Latest Posts</Link>
+                        <Link to='/movies/category/released' onClick={() =>toggleMenu('menu-categories-container')}>Latest Releases</Link>
+                        <Link to='/movies/category/featured' onClick={() =>toggleMenu('menu-categories-container')}>Featured by Administrators</Link>
+                        <Link to='/movies/category/califications' onClick={() =>toggleMenu('menu-categories-container')}>Best Califications</Link>
                     </div>
             </div>
             </div>
@@ -97,7 +102,7 @@ function Header({ userInfo, logOut }){
             <div id='menu-genres-container' className='hidden absolute w-auto h-auto top-6 right-0 bg-amber-100 shadow-lg rounded-sm'>
                     <div className='p-3 h-full text-base flex flex-col gap-5 text-center'>
                         {genresComponents}
-                        <a href='/movies' className='font-bold' onClick={() =>toggleMenu('menu-genres-container')}>More</a>
+                        <Link to='/movies' className='font-bold' onClick={() =>toggleMenu('menu-genres-container')}>More</Link>
                     </div>
             </div>
             </div>
@@ -111,7 +116,7 @@ function Header({ userInfo, logOut }){
             <div id='menu-years-container' className='hidden absolute w-auto h-auto top-6 right-0 bg-amber-100 shadow-lg rounded-sm'>
                     <div className='p-3 h-full text-base flex flex-col gap-5 text-center'>
                         {yearsComponents}
-                        <a href='/movies' className='font-bold' onClick={() =>toggleMenu('menu-years-container')}>More</a>
+                        <Link to='/movies' className='font-bold' onClick={() =>toggleMenu('menu-years-container')}>More</Link>
                     </div>
             </div>
             </div>
@@ -124,27 +129,28 @@ function Header({ userInfo, logOut }){
             {/* User Menu*/}
             <div id='menu-user-container' className='hidden absolute w-auto h-auto top-6 right-0 bg-amber-100 shadow-lg rounded-sm'>
                     <div className='p-3 h-full text-base flex flex-col gap-5 text-center'>
-                        <button className='cursor-pointer font-bold w-15' type='button' title='logOut' onClick={() =>{ toggleMenu('menu-user-container'); handleLogout()
+                        <button className='cursor-pointer w-15 text-red-900' type='button' title='logOut' onClick={() =>{ toggleMenu('menu-user-container'); handleLogout()
                         }}>Log Out</button>
                     </div>
             </div>
             </div>
             </div>
-            {/* Menu button, it only appears when window size is below sm */}
+            
+            {/* Menu button, it only appears when window size is below md */}
             <button type='button' onClick={() =>{
                 toggleMenu('menu-extended-container')
-            }} className='cursor-pointer sm:hidden' title='menu'>
+            }} className='cursor-pointer md:hidden' title='menu'>
                 <IconMenu2 stroke={2} />
             </button>
         </div>
 
          {/* Extended Menu, it only appears when you use the button above */}
             {/* Items inside extended Menu */}
-            <div id='menu-extended-container' className='hidden sm:hidden absolute w-screen h-auto top-17 right-0 left-0 bg-white shadow-lg'>
+            <div id='menu-extended-container' className='hidden md:hidden absolute w-screen h-auto top-17 right-0 left-0 bg-white shadow-lg'>
             <div className='p-7 h-full text-xl flex flex-col gap-5 text-center items-center'>
-            <a href='/home'>Home</a>
-            <a href='/add/movie'>Add Movie</a>
-            <a href='/movies'>Movies</a>
+            <Link to='/home' onClick={() =>toggleMenu('menu-extended-container')}>Home</Link>
+            <Link to='/add/movie' onClick={() =>toggleMenu('menu-extended-container')}>Add Movie</Link>
+            <Link to='/movies' onClick={() =>toggleMenu('menu-extended-container')}>Movies</Link>
             {/* Categories*/}
             <div className='flex flex-col gap-1 items-center'>
             <button onClick={() =>{
@@ -152,11 +158,11 @@ function Header({ userInfo, logOut }){
             }} className='cursor-pointer' type='button' title='categories'>Categories</button>
             <div id='grid-categories-container' className='hidden'>
             <div className='grid grid-cols-2 gap-1 text-sm p-2 bg-amber-100 rounded-2xl'>
-                <a href='/movies/category/views' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>User Views</a>
-                <a href='/movies/category/posted' className=' w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Posts</a>
-                <a href='/movies/category/released' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Releases</a>
-                <a href='/movies/category/featured' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Featured by Administrators</a>
-                <a href='/movies/category/califications' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Best Califications</a>
+                <Link to='/movies/category/views' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>User Views</Link>
+                <Link to='/movies/category/posted' className=' w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Posts</Link>
+                <Link to='/movies/category/released' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Releases</Link>
+                <Link to='/movies/category/featured' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Featured by Administrators</Link>
+                <Link to='/movies/category/califications' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Best Califications</Link>
             </div>
             </div>
             </div>
@@ -167,11 +173,8 @@ function Header({ userInfo, logOut }){
             }} className='cursor-pointer' type='button' title='genres'>Genres</button>
             <div id='grid-genres-container' className='hidden'>
             <div className='grid grid-cols-2 gap-1 text-sm p-2 bg-amber-100 rounded-2xl'>
-                <a href='/movies/category/views' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>User Views</a>
-                <a href='/movies/category/posted' className=' w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Posts</a>
-                <a href='/movies/category/released' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Releases</a>
-                <a href='/movies/category/featured' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Featured by Administrators</a>
-                <a href='/movies/category/califications' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Best Califications</a>
+                {genresComponentsExpanded}
+                <Link to='/movies' className='font-bold w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>More</Link>
             </div>
             </div>
             </div>
@@ -182,11 +185,8 @@ function Header({ userInfo, logOut }){
             }} className='cursor-pointer' type='button' title='years'>Years</button>
             <div id='grid-years-container' className='hidden'>
             <div className='grid grid-cols-2 gap-1 text-sm p-2 bg-amber-100 rounded-2xl'>
-                <a href='/movies/category/views' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>User Views</a>
-                <a href='/movies/category/posted' className=' w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Posts</a>
-                <a href='/movies/category/released' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Latest Releases</a>
-                <a href='/movies/category/featured' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Featured by Administrators</a>
-                <a href='/movies/category/califications' className='w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>Best Califications</a>
+                {yearsComponentsExpanded}
+                <Link to='/movies' className='font-bold w-32 truncate' onClick={() =>toggleMenu('menu-extended-container')}>More</Link>
             </div>
             </div>
             </div>
@@ -197,7 +197,7 @@ function Header({ userInfo, logOut }){
             }} className='cursor-pointer' type='button' title={userInfo.username}>{userInfo.username}</button>
             <div id='grid-user-container' className='hidden'>
             <div className='grid grid-cols-1 gap-1 text-sm p-2 bg-amber-100 rounded-2xl'>
-                <button className='cursor-pointer font-bold w-15' type='button' title='logOut' onClick={() =>{ toggleMenu('menu-extended-container'); handleLogout()
+                <button className='cursor-pointer w-15 text-red-900' type='button' title='logOut' onClick={() =>{ toggleMenu('menu-extended-container'); handleLogout()
                 }}>Log Out</button>
             </div>
             </div>
